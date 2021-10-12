fb3239bf242cb3e4b2a3655578b5f979838fdcc0
Simo Nkosi <wilzmar.kwd@gmail.com>
2021-10-12 20:55:21 +0200

    Apply try/catch to PostData api method.

--- src/api.js
+++ src/api.js
@@ -14,18 +14,22 @@ const getData = async (url) => {
 };
 
 const postData = async (url, data) => {
-  const response = await fetch(url, {
-    method: 'POST',
-    mode: 'cors',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-  });
-  return response.json();
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+    return response.json();
+  } catch (error) {
+    throw new Error(error.message);
+  }
 };
 
-const getShows = async () => getData(' https://api.tvmaze.com/shows');
+const getShows = async () => getData('https://api.tvmaze.com/shows');
 
 const getLikes = async () => getData(`${targetUrl}likes`);
 
