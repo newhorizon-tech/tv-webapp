daa4b32d8accffc69bdc08a2dd4daab69eeeff7f
newhorizon-tech <alaukikinbox@gmail.com>
2021-10-13 16:15:08 +0530

    Find showId

--- src/comments.js
+++ src/comments.js
@@ -3,7 +3,8 @@ import dialogPolyfill from 'dialog-polyfill';
 const dialog = document.querySelector('#dialog');
 dialogPolyfill.registerDialog(dialog);
 
-const displayComments = () => {
+const displayComments = (e) => {
+  const commentId = e.target.parentNode.parentNode.id;
   dialog.showModal();
 };
 
@@ -11,7 +12,7 @@ const startComment = () => {
   const comments = document.querySelectorAll('.comments');
   const closeButton = document.querySelector('#close');
 
-  Array.from(comments, (a) => a.addEventListener('click', displayComments));
+  Array.from(comments, (a) => a.addEventListener('click', (e) => displayComments(e)));
 
   closeButton.addEventListener('click', () => {
     dialog.close();
