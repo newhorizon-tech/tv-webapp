5c30854dc58a62414ea4cb9497156f028cf47647
newhorizon-tech <alaukikinbox@gmail.com>
2021-10-14 23:03:44 +0530

    Add initial comment constants

--- src/tests/comments-counter.test.js
+++ src/tests/comments-counter.test.js
@@ -5,9 +5,15 @@
 import commentsCounter from '../comments-counter';
 
 const comment1 = document.createElement('li');
-comment1.textContent = 'One';
+comment1.className = 'comment';
 const comment2 = document.createElement('li');
-comment1.textContent = 'One';
+comment2.className = 'comment';
+const comment3 = document.createElement('li');
+comment3.className = 'comment';
+const comment4 = document.createElement('li');
+comment4.className = 'comment';
+const comment5 = document.createElement('li');
+comment5.className = 'comment';
 
 describe('Testing Comments Counter', () => {
   test('Testing if it works', () => {
