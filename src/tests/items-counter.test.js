7309a6011458d3364f3f68fc0fa73c3faff6be02
Simo Nkosi <wilzmar.kwd@gmail.com>
2021-10-15 10:32:54 +0200

    Add two more test for tv show cards.

--- src/tests/items-counter.test.js
+++ src/tests/items-counter.test.js
@@ -14,3 +14,33 @@ describe('checks for items counter', () => {
     expect(result).toEqual(expect.any(Number));
   });
 });
+
+describe('checks for card elements', () => {
+  test('One show', () => {
+    document.body.innerHTML = '';
+
+    const testDiv = document.createElement('div');
+    testDiv.className = 'tv-show-card';
+
+    document.body.append(testDiv);
+    const showCount = itemsCounter();
+
+    expect(showCount).toEqual(1);
+  });
+
+  test('two shows', () => {
+    document.body.innerHTML = '';
+
+    const testDiv = document.createElement('div');
+    testDiv.className = 'tv-show-card';
+    document.body.append(testDiv);
+
+    const testDiv2 = document.createElement('div');
+    testDiv2.className = 'tv-show-card';
+    document.body.append(testDiv2);
+
+    const showCount = itemsCounter();
+
+    expect(showCount).toEqual(2);
+  });
+});
